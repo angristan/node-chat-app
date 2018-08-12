05aada57dd6200447400fca136841cf93e0d0155
Angristan <angristan@pm.me>
2018-08-12 23:08:14 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to the server');
-
-  socket.emit('createMessage', {
-    from: 'Andrew',
-    text: 'Yay!'
-  })
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'Hey',
-    createdAt: '123',
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime(),
+    });
   });
 
   socket.on('disconnect', () => {

3c4ab7bdff393514bb2263b8b58b39d50b649aea
Angristan <angristan@pm.me>
2018-08-12 23:25:07 +0200

    Greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat room',
+    createdAt: new Date().getTime(),
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'A new user joined',
+    createdAt: new Date().getTime(),
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
 

77ee746a3a9ed0a88674a83ed6405fa1b3adc0f2
Angristan <angristan@pm.me>
2018-08-12 18:26:34 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}`);
+});

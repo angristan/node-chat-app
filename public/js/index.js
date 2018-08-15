6bd9a34c144443b724ef65377184674416325d45
Angristan <angristan@pm.me>
2018-08-15 11:52:50 +0200

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,10 +10,15 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   const formattedTime = moment(message.createdAt).format('h:mm a');
-  const li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
-  jQuery('#messages').append(li);
+  const template = jQuery('#message-template').html();
+  const html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime,
+  });
+
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {

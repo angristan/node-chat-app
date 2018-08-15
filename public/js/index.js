638e7dbe6fac55f8fe0b9ec4e6bfd4ce2d231ef6
Angristan <angristan@pm.me>
2018-08-15 23:01:08 +0200

    Handle auto-scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 const socket = io();
 
+function scrollToBottom() {
+  const messages = jQuery('#messages');
+  const newMessage = messages.children('li:last-child');
+
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+};
+
 socket.on('connect', function () {
   console.log('Connected to the server');
 });
@@ -19,6 +34,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
